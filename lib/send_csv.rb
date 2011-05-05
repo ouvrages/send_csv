3a13dcab3c057343afbdb4a188607a38a563ee6c
Michael Witrant <mike@lepton.fr>
2011-05-05 12:57:30 +0200

    generated gem with hoe

---
+++ lib/send_csv.rb
@@ -0,0 +1,3 @@
+class SendCsv
+  VERSION = '1.0.0'
+end
---
+++ test/test_send_csv.rb
@@ -0,0 +1,8 @@
+require "test/unit"
+require "send_csv"
+
+class TestSendCsv < Test::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
