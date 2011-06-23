785af5ccf2ec5ced5c8f26f55d75f9d45ab2e5b0
Michael Witrant <mike@lepton.fr>
2011-06-23 18:10:23 +0200

    added generate_csv method

--- lib/send_csv.rb
+++ lib/send_csv.rb
@@ -1,5 +1,5 @@
 module SendCsv
-  VERSION = '0.2'
+  VERSION = '0.3'
 end
 
 # Based on http://stackoverflow.com/questions/3468858/rails-3-0-engine-execute-code-in-actioncontroller/3484141#3484141
@@ -9,9 +9,19 @@ require 'action_controller'  # Make sure ActionController::Base is defined
 ActionController::Base.class_eval do
   private
 
-  def send_csv(lines, options = {})
+  def generate_csv(lines, options = {})
     require 'csv'
     
+    options = options.dup
+    encoding = options.delete(:encoding) || 'ISO-8859-1'
+    
+    csv = lines.map { |values| CSV.generate_line(values, ';') + "\r\n" }.join
+    csv = Iconv.conv(encoding, 'utf-8', csv)
+    
+    csv
+  end
+
+  def send_csv(lines, options = {})
     options = options.dup
     encoding = options.delete(:encoding) || 'ISO-8859-1'
     
@@ -20,8 +30,7 @@ ActionController::Base.class_eval do
       :type => 'text/plain',
     }.merge(options)
     
-    csv = lines.map { |values| CSV.generate_line(values, ';') + "\r\n" }.join
-    csv = Iconv.conv(encoding, 'utf-8', csv)
+    csv = generate_csv(lines, options)
     
     send_data csv, options
   end
--- send_csv.gemspec
+++ send_csv.gemspec
@@ -2,17 +2,15 @@
 
 Gem::Specification.new do |s|
   s.name = %q{send_csv}
-  s.version = "0.2"
+  s.version = "0.3"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Ouvrages"]
-  s.date = %q{2011-05-05}
-  s.default_executable = %q{send_csv}
+  s.date = %q{2011-06-23}
   s.description = %q{Adds to ApplicationController a send_csv method working like send_file.}
   s.email = ["contact@ouvrages-web.fr"]
-  s.executables = ["send_csv"]
   s.extra_rdoc_files = ["History.txt", "Manifest.txt", "README.txt"]
-  s.files = [".autotest", "History.txt", "Manifest.txt", "README.txt", "Rakefile", "bin/send_csv", "lib/send_csv.rb", "test/test_send_csv.rb", ".gemtest"]
+  s.files = [".autotest", "History.txt", "Manifest.txt", "README.txt", "Rakefile", "lib/send_csv.rb", "test/test_send_csv.rb", ".gemtest"]
   s.homepage = %q{https://github.com/ouvrages/send_csv}
   s.rdoc_options = ["--main", "README.txt"]
   s.require_paths = ["lib"]
